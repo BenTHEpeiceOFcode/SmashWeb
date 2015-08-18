fd4e68aa99bd9e760710a4e0a0391187d6be4632
Ben Johnston <smashlols@outlook.com>
2015-08-18 11:47:54 +0100

    Added home page

---
+++ Home/index.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SmashWeb - Home</title>
+</head>
+<body>
+<center>
+	<h1>SmashWeb - Home</h1>
+	<p>SmashWeb is a nice little HTML and PHP peice of software.</p>
+</center>
+</body>
+</html>
